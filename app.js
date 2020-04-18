2e4f1472679001543dc3c5ca5b794256cc9aec2a
原田大輔 <haradadaisuke@haradadaisukenoMacBook-Pro.local>
2020-04-18 23:38:05 +0900

    [update]6

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(function(){
-	$('.box1').slideDown(function () {
-	$('.box1').css({
-		'background-color':'#0000FF',
-		'width':'200px',
-		'height':'100px',
-	}).slideUp();
-  });
+	$('.bg1').on('click',function(){
+		$('.bg1').slideUp();
+	});
+
+	$('.bg2').on('click',function(){
+		$('.bg2').slideUp();
+	});
+
+	$('.bg3').on('click',function(){
+		$('.bg3').slideUp();
+		// body...
+	});
+
+	$('.bg4').on('click',function(){
+		$('.bg4').slideUp();
+	});
 });
